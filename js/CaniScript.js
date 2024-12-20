9c96638e86723e079251b351a4715b6991e8a4dc
daniel tonin <122627482+Tonino01@users.noreply.github.com>
2024-12-20 15:08:53 +0100

    v1

---
+++ js/CaniScript.js
@@ -0,0 +1,25 @@
+const carlinoSrc = ".\img\carlino.jpg";
+
+const basenjiSrc = ".\img\basenji.jpg";
+
+const sanbernardoSrc = ".\img\san bernardo.jpg";
+
+const terranovaSrc = ".\img\terranova.jpg";
+
+const vettImg = [carlinoSrc, basenjiSrc, sanbernardoSrc, terranovaSrc];
+
+
+const vettDescrizione = ["basenji (Congo, 43 cm)", "carlino (Cina, 30 cm)", "san bernardo (Svizzera, 80 cm)", "terranova (Canada, 75 cm)"];
+
+
+function mostra(){
+	
+	const cane = document.createElement("div");
+	
+	const img = document.createElement("img");
+	
+	const p = document.createElement("p");
+	
+	img.src = 
+	
+}
